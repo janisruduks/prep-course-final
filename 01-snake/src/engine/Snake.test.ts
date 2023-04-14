027ba68abc7052f5c4e3e0b15b13eb3133678958
Erkyill <janis.ruduks157@gmail.com>
2023-04-14 19:21:54 +0300

    tests passed

--- 01-snake/src/engine/Snake.test.ts
+++ 01-snake/src/engine/Snake.test.ts
@@ -20,11 +20,13 @@ describe("Snake", () => {
     const snake = new Snake();
 
     snake.setDirection("Right");
-    snake.move();
-    snake.move()
     snake.setDirection("Left")
+    snake.move();
+    snake.move();
+    snake.setDirection("Left")
+    snake.move();
 
-    expect(snake.getHead()).toEqual(new Cell(4, 0));
+    expect(snake.getHead()).toEqual(new Cell(5, 0));
   });
   it("should be able to move down and bend the tail", () => {
     const snake = new Snake();
@@ -34,5 +36,18 @@ describe("Snake", () => {
 
     expect(snake.getHead()).toEqual(new Cell(2, 1));
     expect(snake.getTail()).toEqual([new Cell(1, 0), new Cell(2, 0)]);
+  });
+  it('should grow when it eats an apple', () => { // will need to update this test after seed()
+    const snake = new Snake();
+    snake.head = new Cell(18, 16); //apple position
+    snake.grow();
+
+    expect(snake.tail.length).toBe(3);
+  });
+  it("Shouldn't be able to eat itself", () => {
+    const snake = new Snake();
+    snake.head = new Cell(1, 0);
+
+    expect(snake.isTakenBySnake(snake.head)).toBe(true);
   });
 });
