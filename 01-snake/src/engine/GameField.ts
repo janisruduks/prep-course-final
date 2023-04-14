5ebf18d25c247294a7a36c18948effb49dae0aed
Erkyill <janis.ruduks157@gmail.com>
2023-04-14 19:23:00 +0300

    apples need to be randomized on field

--- 01-snake/src/engine/GameField.ts
+++ 01-snake/src/engine/GameField.ts
@@ -1,27 +1,45 @@
 import { Cell } from "./Cell";
 
 export class GameField {
-  /**
-   * Called when level completed
-   */
-  seed(): void {}
+  apples: Cell[]  
 
-  getApples(): Cell[] {
-    return [
+  constructor(){
+    this.apples = [    
       new Cell(18, 16),
       new Cell(20, 16),
       new Cell(22, 16),
-      new Cell(24, 16)
-    ];
+      new Cell(24, 16) ];
+  }
+
+  seed(): void {}
+
+  getApples(): Cell[] {
+    return this.apples
   }
 
   isAppleInside(cell: Cell): boolean {
+    for(const apple of this.apples) {
+      if (cell.x === apple.x && cell.y === apple.y) {
+        return true;
+      }
+    }
     return false;
   }
 
-  removeApple(cell: Cell): void {}
+  removeApple(cell: Cell): void {
+    for(const apple of this.apples) {
+      const index = this.apples.indexOf(apple)
+      if (cell.x === apple.x && cell.y === apple.y) {
+        this.apples.splice(index, 1);
+      }
+    }
+  }
 
   isEmpty(): boolean {
-    return false;
+    if(this.apples.length === 0){
+      return true;
+    }else {
+      return false;
+    }
   }
 }
